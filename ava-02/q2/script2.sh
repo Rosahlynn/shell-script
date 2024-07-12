a01fdc4b07407c8c24df631a48fde42323c963d1
Mariane Andrade <117303716+Rosahlynn@users.noreply.github.com>
2024-07-12 09:55:24 -0300

    script2.sh

--- ava-02/q2/script2.sh
+++ ava-02/q2/script2.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
+agendamento() {
+  op=$(yad --title "Agendamento de Backup" \
+           --form \
+           --field="Hora e Minuto:H" "$(date +%H:%M)" \
+           --field="Comando de Backup:C" "/home/ava-02/q1/script.sh" \
+           --button="Agendar:0" \
+           --button="Cancelar:1")
 
-agendamento () {
- op=$(yad --title "Agendamento de Backup" \
-	  --form \
-	  --field="Hora e Minuto:H" "$(date +%H:%M)" \
-	  --field="Comando de Backup:C" "/home/ava-02/q1/script.sh"
-	  --button="Agendar:0"
-	  --button="Cancelar:1"
+  if [ $? -eq 0 ]; then
+    ti=$(echo "$op" | cut -d'|' -f1)
+    da=$(echo "$op" | cut -d'|' -f2)
+    bp=$(echo "$op" | cut -d'|' -f3)
 
- if [ $? -eq 0 ]; then
-   ti=$(echo "op" | cut -d'|' -f1)
-   da=$(echo "op" | cut -d '|' -f2
-   bp=$(echo "op" | cut -d '|' -f3
-
-   go=$(date -d "$da $ti" +"%H:%M %m%d%Y")
+    go=$(date -d "$da $ti" +"%H:%M %m%d%Y")
 
     echo "$bp" | at "$go"
 
     echo "Agendamento concluído"
 
- else
-   echo "Agendamento cancelado"
- fi
+  else
+    echo "Agendamento cancelado"
+  fi
 }
